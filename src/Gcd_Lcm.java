6b4dab73c5f43b6fdacc60a492d189618bd8f867
aryan <aryanupadhyay647@gmail.com>
2025-09-03 13:33:58 +0530

    Gcd

--- src/Gcd_Lcm.java
+++ src/Gcd_Lcm.java
@@ -1,11 +1,15 @@
 public class Gcd_Lcm {
     public static void main(String[] args) {
-        System.out.println(gcd(4,9));
+        System.out.println(gcd(6,18));
+        System.out.println(lcm(9,18));
     }
     static int gcd(int a  , int b){
         if(a==0){
             return b;
         }
         return gcd(b%a , a);
+    }
+    static int lcm(int a , int b){
+        return a * b / gcd(a,b);
     }
 }
