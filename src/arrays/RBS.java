8d5e2f41e771c43297411b98ae64b00c46cb5a99
aryan <aryanupadhyay647@gmail.com>
2025-09-11 16:12:01 +0530

    Recursion

---
+++ src/arrays/RBS.java
@@ -0,0 +1,30 @@
+package arrays;
+
+public class RBS {
+    public static void main(String[] args) {
+        int [] arr = {5,6,7,8,9,1,2,3};
+        System.out.println(rbs(arr,4,0,arr.length-1));
+    }
+    static int rbs(int[] arr, int target , int s, int e){
+        if(s>e){
+            return -1;
+        }
+        int mid = s + (e-s)/2;
+        if(target == arr[mid]){
+            return mid;
+        }
+        if(arr[s] <= arr[mid]){
+            if(target >= arr[s] && target<= arr[mid]){
+                return rbs(arr,target,s,mid-1);
+            }
+            else{
+                return rbs(arr,target,mid+1 , e);
+            }
+
+        }
+        if(target>=arr[mid]&&target<=arr[e]){
+            return rbs(arr,target,mid+1,e);
+        }
+        return rbs(arr,target,s,mid-1);
+    }
+}
